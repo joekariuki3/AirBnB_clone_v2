49e995561d8eae3e99952e845ad30338353bd83f
joekariuki3 <joekariuki3@gmail.com>
2023-10-06 15:58:31 +0300

    Prepare your web servers give right permission to index file

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -7,6 +7,7 @@ sudo mkdir -p /data/web_static/shared/
 sudo mkdir -p /data/web_static/releases/test/
 sudo ln -f -s /data/web_static/releases/test/ /data/web_static/current
 sudo chown -R ubuntu:ubuntu /data/
+sudo chown ubuntu:ubuntu /data/web_static/releases/test/index.html
 echo "Sample text" > /data/web_static/releases/test/index.html
 sudo chown -R ubuntu:ubuntu /etc/nginx/sites-available/default
 sudo sed -i 's#root /var/www/html;#root /var/www/html;\n\n\tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}#1' /etc/nginx/sites-available/default
