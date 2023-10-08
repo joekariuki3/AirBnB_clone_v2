510d15b2d5f5a6e89af4de46a76a19b9dc034d26
joekariuki3 <joekariuki3@gmail.com>
2023-10-08 15:03:52 +0300

    prepair server for deployment with puppet correction 5

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,55 +1,51 @@
 # puppet manifest to prepare new server for deployment
-include stdlib
-
 # Update package lists
 exec { 'Update lists':
-    command => '/usr/bin/apt update'
+  command => 'sudo /bin/apt update'
 }
 
 # Install Nginx
 package { 'nginx':
-    ensure  => 'present',
-    require => Exec['Update lists']
+  ensure  => 'present',
+  require => Exec['Update lists']
 }
 
 # Create the directory tree
 exec { 'Create Directory Tree':
-    command => '/bin/mkdir -p /data/web_static/releases/test /data/web_static/shared',
-    require => Package['nginx']
+  command => 'sudo /bin/mkdir -p /data/web_static/releases/test /data/web_static/shared',
+  require => Package['nginx']
 }
-
-$data = 'Sample Text'
 
 # Create a fake HTML file with simple content,
 # to test Nginx configuration
 file { 'Create Fake HTML':
-    ensure  => 'present',
-    path    => '/data/web_static/releases/test/index.html',
-    content => $data,
-    require => Exec['Create Directory Tree']
+  ensure  => 'present',
+  path    => '/data/web_static/releases/test/index.html',
+  content => 'Sample Text',
+  require => Exec['Create Directory Tree']
 }
 
 # Create a symbolic link '/data/web_static/current' linked to the
 # '/data/web_static/releases/test/' folder.
 file { 'Create Symbolic Link':
-    ensure  => 'link',
-    path    => '/data/web_static/current',
-    force   => true,
-    target  => '/data/web_static/releases/test',
-    require => File['Create Fake HTML']
+  ensure  => 'link',
+  path    => '/data/web_static/current',
+  force   => true,
+  target  => '/data/web_static/releases/test',
+  require => File['Create Fake HTML']
 }
 
 # Ensures that Nginx is running
 service { 'nginx':
-    ensure  => 'running',
-    enable  => true,
-    require => Package['nginx']
+  ensure  => 'running',
+  enable  => true,
+  require => Package['nginx']
 }
 
 # Set permissions for 'ubuntu' user
 exec { 'Set permissions':
-    command => '/bin/chown -R ubuntu:ubuntu /data',
-    require => File['Create Symbolic Link']
+  command => 'sudo /bin/chown -R ubuntu:ubuntu /data',
+  require => File['Create Symbolic Link']
 }
 
 # Set a new location for a Nginx VHost 
@@ -59,10 +55,13 @@ $new_location="\n\t${loc_header}\n\t\t${loc_content}\n\t}\n"
 
 # Write the new location to the default Nginx VHost
 file_line { 'Set Nginx Location':
-    ensure  => 'present',
-    path    => '/etc/nginx/sites-available/default',
-    after   => 'root /var/www/html;',
-    line    => $new_location,
-    notify  => Service['nginx'],
-    require => Exec['Set permissions']
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'root /var/www/html;',
+  line    => $new_location,
+  notify  => Service['nginx'],
+  require => Exec['Set permissions']
+}
+exec { 'restart-nginx':
+  command => '/bin/service nginx restart',
 }
